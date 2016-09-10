779519e945866dcaf82c194e5b0dec4fc56e60cb
moriahtobin <moriah.n.tobin@gmail.com>
2016-09-10 01:27:04 -0500

    Functional test now checks we can input a to-do list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
         #Saria happily notes that the page title mentions "to-do lists"
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-#The page prompts her to enter a to-do item
+        #The page prompts her to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-#She types "Teach Link Awesome New Orcarina Song" into the text box
+        #She types "Teach Link Awesome New Orcarina Song" into the text box
+        inputbox.send_keys('Teach Link Awesome New Orcarina Song')
 
-#When she types enter, the page updates, and now the page lists
-#"1: Teach Link awesome new orcarina song" as an item in a to-do list
+        #When she types enter, the page updates, and now the page lists
+        #"1: Teach Link awesome new orcarina song" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-#There is still a text box for adding an additional item to the list
-#She enters "Check out decrepit Forest Temple"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Teach Link Awesome New Orcarina Song' for row in rows)
+        )
+        #There is still a text box for adding an additional item to the list
+        #She enters "Check out decrepit Forest Temple"
+        self.fail('Finish the test!')
 
 #The page updates again showing both items
 
